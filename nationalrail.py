51d82e131052cf474231f303be6fd41fbc1b44a6
Mark McLaughlin <mark@mclaughlin.me.uk>
2021-12-29 23:51:30 -0800

    Black formatter

--- nationalrail.py
+++ nationalrail.py
@@ -1,14 +1,12 @@
 """Display plain-text table of upcoming departures from a named station."""
+from urllib.parse import urljoin
 import requests
 import json
 import click
-
-from urllib.parse import urljoin
-
 import tabulate as tb
 from tabulate import tabulate
-tb.PRESERVE_WHITESPACE = True
 
+tb.PRESERVE_WHITESPACE = True
 
 API = "https://huxley2.azurewebsites.net"
 
@@ -26,7 +24,7 @@ def get_train_services(crs: str, endpoint: str, realtime: bool = False) -> dict:
             raise SystemExit
     else:
         try:
-            with open(f"./json/{crs.lower()}.json") as f:
+            with open(f"./json/{crs.lower()}.json", encoding="utf-8") as f:
                 train_services = json.load(f)
         except FileNotFoundError:
             print(f"Error: No JSON file found for {crs}")
@@ -38,33 +36,44 @@ def get_departure_board(crs: str, realtime: bool = True) -> list:
     """Get departure board for a given CRS code."""
     departures = []
     services = get_train_services(crs, "departures", realtime)
-    train_services = services['trainServices']
+    train_services = services["trainServices"]
 
     if train_services != None:
-        for train in train_services:
-            destination = train['destination'][0]['locationName']
+        for service in train_services:
+            destination = service["destination"][0]["locationName"]
 
-            if train['isCancelled'] == True:
-                cancel_reason = train['cancelReason']
+            if service["isCancelled"] == True:
+                cancel_reason = service["cancelReason"]
                 destination = f"{destination}\n{cancel_reason}"
 
             departures.append(
-                [train['std'], destination.ljust(32),
-                 train['platform'], train['etd']])
+                [
+                    service["std"],
+                    destination.ljust(32),
+                    service["platform"],
+                    service["etd"],
+                ]
+            )
     else:
         departures.append(["", "No scheduled departures", "", ""])
     return departures
 
 
-@ click.command()
-@ click.option('--crs', default="wok", help='CRS code for station.')
-@ click.option('--realtime', default=True, help='Use realtime data.')
+@click.command()
+@click.option("--crs", default="wok", help="CRS code for station.")
+@click.option("--realtime", default=True, help="Use realtime data.")
 def departures(crs: str, realtime: bool):
     """Display plain-text table of upcoming departures from a named station."""
     departures = get_departure_board(crs, realtime)
 
-    print(tabulate(departures, headers=["Time", "Destination", "Plat", "Expected"],
-                   colalign=("left", "left", "right", "right"), tablefmt="simple"))
+    print(
+        tabulate(
+            departures,
+            headers=["Time", "Destination", "Plat", "Expected"],
+            colalign=("left", "left", "right", "right"),
+            tablefmt="simple",
+        )
+    )
 
 
 if __name__ == "__main__":
