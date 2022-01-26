ad094aa8a342619a89b2d9f038a3b15a70d53a31
Mark McLaughlin <mark@mclaughlin.me.uk>
2022-01-25 17:49:29 -0800

    Center NRCC messages in regular table width

--- cli.py
+++ cli.py
@@ -48,7 +48,7 @@ def get_departures(crs: str) -> None:
         if station.nrcc_messages:
             for message in station.nrcc_messages:
                 output = Padding(message, (0, 4, 1, 4))
-                console.print(output, style=yellow, justify="center")
+                console.print(output, style=yellow, justify="center", width=60)
         else:
             no_services: str = "Please check timetable for services\n"
             console.print(no_services, style=yellow, justify="center")
